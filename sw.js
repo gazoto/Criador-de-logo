c317350c80f880254c059547a54f700e31f968da
gazoto <gabrielverzoto@hotmail.com>
2025-06-07 23:03:04 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'logo-maker-cache-v1';
+const urlsToCache = [
+  '.',
+  'index.html'
+  // Adicione aqui outros arquivos que você queira que funcionem offline
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
